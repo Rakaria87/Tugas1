18c375d4cb6e7fa52ba77a77b460257018ec30f4
Rakaria87 <azrael.zhia.ariaraka@gmail.com>
2025-04-27 18:34:50 +0700

    Add files via upload

---
+++ list_nama.py
@@ -0,0 +1,6 @@
+Takjil = ["Kolak Pisang", "Kurma", "Gorengan", "Es Campur", "Es Cendol"]
+print ("Menu Takjil Hari Ini:", Takjil)
+
+Takjil.insert (3, "Piscok")
+Takjil.remove ("Es Campur")
+print ("Spesial Hari Ini:", Takjil)
